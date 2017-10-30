a86144679163c2abd0b6ea05523f9d41133cabb9
Cansın Aldanmaz <cnsn@outlook.com>
2017-10-30 22:46:15 +0300

    first commit

--- Modules/Automagic/InvokeTests.ps1
+++ Modules/Automagic/InvokeTests.ps1
@@ -1,4 +1,5 @@
 Install-Module Pester -Force
+$Output = Join-Path "$PSScriptRoot\Tests" TestsResults.xml
 $res = Invoke-Pester -Path "$PSScriptRoot\Tests" -OutputFormat NUnitXml -OutputFile $Output -PassThru 
 if ($res.FailedCount -gt 0) { 
 	throw "$($res.FailedCount) unit tests failed."
